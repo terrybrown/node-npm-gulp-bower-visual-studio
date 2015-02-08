008af0730a8215dbbefd9f57ef43ba78349a38b1
Terry Brown <terry@hairylemon.net>
2015-02-08 09:14:27 +0000

    generating sourcemaps for appropriate outputs

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,15 @@
-/// <vs Clean='clean' SolutionOpened='watch' />
-var gulp = require('gulp');
-var del = require('del');
-var concat = require('gulp-concat');
-var less = require('gulp-less');
-var uglify = require('gulp-uglify');
-var watch = require('gulp-watch');
+/// <vs Clean='clean' SolutionOpened='build-and-watch' />
+var gulp		= require('gulp');
+var del			= require('del');
+var concat		= require('gulp-concat');
+var less		= require('gulp-less');
+var uglify		= require('gulp-uglify');
+var watch		= require('gulp-watch');
+var sourcemaps	= require('gulp-sourcemaps');
+var path		= require('path');
+var flatten		= require('gulp-flatten');
 
-var outputLocation = 'dist';
+var outputLocation = './dist';
 
 
 gulp.task('clean', function () {
@@ -22,21 +25,31 @@ gulp.task('vendor-scripts', function () {
 					'bower_components/respond/dest/respond.min.js']
 	};
 
+	var vendorOutput = path.join(outputLocation, 'vendor/scripts');
+
 	gulp.src(vendorSources.jquery)
 		.pipe(concat('jquery.bundle.min.js'))
-		.pipe(gulp.dest(outputLocation + '/vendor/scripts/'));
+		.pipe(gulp.dest(vendorOutput));
 
 	gulp.src(vendorSources.other)
 		.pipe(concat('other.bundle.min.js'))
-		.pipe(gulp.dest(outputLocation + '/vendor/scripts/'));
+		.pipe(gulp.dest(vendorOutput));
 
 	gulp.src('bower_components/modernizr/modernizr.js')
-		.pipe(gulp.dest(outputLocation + '/vendor/scripts/'));
+		.pipe(sourcemaps.init())
+		.pipe(sourcemaps.write())
+		.pipe(gulp.dest(vendorOutput));
+
+	gulp.src('bower_components/**/*.min.map')
+		.pipe(flatten())
+		.pipe(gulp.dest(vendorOutput));
 });
 
 gulp.task('vendor-css', function () {
 			  
 	gulp.src('bower_components/bootstrap/dist/css/*.min.css')
+		.pipe(sourcemaps.init({loadMaps: true}))
+		.pipe(sourcemaps.write())
 		.pipe(gulp.dest(outputLocation + '/vendor/css'));
 
 	gulp.src('bower_components/bootstrap/dist/fonts/**/*')
@@ -44,12 +57,14 @@ gulp.task('vendor-css', function () {
 });
 
 gulp.task('css', function () {
-	gulp.src('Content/**/*.less')
+	gulp.src(['./Content/**/*.less', '!./Content/**/_*.less'])
+		.pipe(sourcemaps.init())
 		.pipe(less({
 			compress: true
 		}))
+		.pipe(sourcemaps.write())
 		.pipe(concat('compiled.min.css'))
-		.pipe(gulp.dest(outputLocation + '/css'));
+		.pipe(gulp.dest(path.join(outputLocation, 'css')));
 });
 
 gulp.task('watch', function () {
@@ -57,6 +72,8 @@ gulp.task('watch', function () {
 	gulp.watch('Content/**/*.less', ['css']);
 });
 
-gulp.task('default', ['clean', 'vendor-scripts', 'vendor-css', 'css', 'watch'], function(){});
+gulp.task('build-and-watch', ['default', 'watch'], function () {});
+
+gulp.task('default', ['clean', 'vendor-scripts', 'vendor-css', 'css'], function(){});
 
 
